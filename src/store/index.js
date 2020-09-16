de839bea36db9fd4e4cf61c198d29aa13dd5b509
Peterg <petercharles.ca@gmail.com>
2020-09-15 23:53:43 -0400

    Checkout

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,8 @@ export default new Vuex.Store({
   state: {
     products: [],
     // {id, quantity}
-    cart: []
+    cart: [],
+    checkoutStatus: null
   },
 
   getters: {
@@ -57,6 +58,19 @@ export default new Vuex.Store({
         }
         context.commit('decrementProductInventory', product)
       }
+    },
+
+    checkout({state, commit}) {
+      shop.buyProducts(
+        state.cart,
+        () => {
+          commit('emptyCart')
+          commit('setCheckoutStatus', 'success')
+        },
+        () => {
+          commit('setCheckoutStatus', 'fail')
+        }
+      )
     }
   },
 
@@ -80,6 +94,14 @@ export default new Vuex.Store({
 
     decrementProductInventory(state, product) {
       product.inventory--
+    },
+
+    setCheckoutStatus(state, status) {
+      state.checkoutStatus = status
+    },
+
+    emptyCart(state) {
+      state.cart = []
     }
 
   }
