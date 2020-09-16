2f85854bdbb820891ffd0367eb10ef749b28a05a
Peterg <petercharles.ca@gmail.com>
2020-09-16 13:02:14 -0400

    Split Vuex Store in Multiple Files

---
+++ src/store/action.js
@@ -0,0 +1,39 @@
+import shop from "@/api/shop";
+
+export default
+{
+  fetchProducts({commit}) {
+    return new Promise((resolve, reject) => {
+      shop.getProducts(products => {
+        commit('setProducts', products),
+        resolve()
+      })
+    })
+  },
+
+  addProductToCart({state, getters, commit}, product) {
+    // if (product.inventory > 0) {
+    if (getters.productIsInStock(product)) {
+      const cartItem = state.cart.find(item => item.id === product.id)
+      if (!cartItem) {
+        commit('pushProductToCart', product.id)
+      } else {
+        commit('incrementItemQuantity', cartItem)
+      }
+      commit('decrementProductInventory', product)
+    }
+  },
+
+  checkout({state, commit}) {
+    shop.buyProducts(
+      state.cart,
+      () => {
+        commit('emptyCart')
+        commit('setCheckoutStatus', 'success')
+      },
+      () => {
+        commit('setCheckoutStatus', 'fail')
+      }
+    )
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vuex, { Store } from 'vuex';
 import Vue from 'vue'
-import shop from '@/api/shop';
+// import shop from '@/api/shop';
+import actions  from "./action";
 
 Vue.use(Vuex)
 
@@ -44,42 +45,43 @@ export default new Vuex.Store({
     }
   },
 
-  actions: {
-    fetchProducts({commit}) {
-      return new Promise((resolve, reject) => {
-        shop.getProducts(products => {
-          commit('setProducts', products),
-          resolve()
-        })
-      })
-    },
+  // actions: actions /* {
+  //   fetchProducts({commit}) {
+  //     return new Promise((resolve, reject) => {
+  //       shop.getProducts(products => {
+  //         commit('setProducts', products),
+  //         resolve()
+  //       })
+  //     })
+  //   },
 
-    addProductToCart({state, getters, commit}, product) {
-      // if (product.inventory > 0) {
-      if (getters.productIsInStock(product)) {
-        const cartItem = state.cart.find(item => item.id === product.id)
-        if (!cartItem) {
-          commit('pushProductToCart', product.id)
-        } else {
-          commit('incrementItemQuantity', cartItem)
-        }
-        commit('decrementProductInventory', product)
-      }
-    },
+  //   addProductToCart({state, getters, commit}, product) {
+  //     // if (product.inventory > 0) {
+  //     if (getters.productIsInStock(product)) {
+  //       const cartItem = state.cart.find(item => item.id === product.id)
+  //       if (!cartItem) {
+  //         commit('pushProductToCart', product.id)
+  //       } else {
+  //         commit('incrementItemQuantity', cartItem)
+  //       }
+  //       commit('decrementProductInventory', product)
+  //     }
+  //   },
 
-    checkout({state, commit}) {
-      shop.buyProducts(
-        state.cart,
-        () => {
-          commit('emptyCart')
-          commit('setCheckoutStatus', 'success')
-        },
-        () => {
-          commit('setCheckoutStatus', 'fail')
-        }
-      )
-    }
-  },
+  //   checkout({state, commit}) {
+  //     shop.buyProducts(
+  //       state.cart,
+  //       () => {
+  //         commit('emptyCart')
+  //         commit('setCheckoutStatus', 'success')
+  //       },
+  //       () => {
+  //         commit('setCheckoutStatus', 'fail')
+  //       }
+  //     )
+  //   }
+  // }*/,
+  actions,
 
   mutations: {
     setProducts(state, products) {
